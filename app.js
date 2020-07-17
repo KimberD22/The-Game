a6af3f9c1cd1a4f42db6552b63feec3761215d41
KimberD22 <kimberdepriest@gmail.com>
2020-07-16 20:45:48 -0400

    updates

---
+++ app.js
@@ -0,0 +1,3 @@
+alert("Do you love donuts or pizza? It is time to pick a side, Dougie and Pizza Rat are at it again, arguing over which dough is the best. Would you like to join in the fun. Play a few rounds of tic tac dough to see who can claim they have the best dough if you want to play just press ok.")
+/*onst playerOne = Dougie
+const playerTwo = Pizza Rat */
